f23d42d1aed78e734f7f3bb467bbde0f1c5e4788
Paulo Ancheta <paulo.ancheta@gmail.com>
2015-04-17 11:59:59 -0700

    added collision detection

--- app/assets/javascripts/movement.js
+++ app/assets/javascripts/movement.js
@@ -1,7 +1,30 @@
 $(document).ready(function(){
-  var ball = $('#main_ball');
+  var ball = $('#main_ball'),
+     block = $('#block');
 
-  $(this).on('keyup', function(e) {
+  var didCollide = function( main_character, enemy ){
+    if(typeof(enemy.offset()) !== "undefined" && typeof(enemy.offset()) !== "undefined"){
+      var enemy_x1 = enemy.offset().left;
+      var enemy_x2 = enemy.offset().left + enemy.width();
+      var enemy_y1 = enemy.offset().top;
+      var enemy_y2 = enemy.offset().top + enemy.width();
+
+      // since my ball is has a padding from image it needs to be subtracted
+      var main_char_x1 = main_character.offset().left - 10;
+      var main_char_x2 = main_character.offset().left + main_character.width() - 10;
+      var main_char_y1 = main_character.offset().top - 10;
+      var main_char_y2 = main_character.offset().top + main_character.width() - 10;  
+
+      if(enemy_x1 < main_char_x2 && enemy_x2 > main_char_x1 ){ // check horizontal collision
+        if(enemy_y1 < main_char_y2){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  
+  $(this).on('keydown', function(e) {
     if(e.which === 32){
       TweenLite.to( ball , 0.5 , {
         bottom: "+=200px",
@@ -14,11 +37,26 @@ $(document).ready(function(){
 
   $(this).on('keydown', function(e){
     if(e.which === 39){
-      TweenLite.to( ball , 1 , {left:"+=50px"} );
+      var move_left = TweenLite.to( ball , 1 , { 
+        left: "+=200px",
+        onUpdate: function(){
+          if( didCollide(ball, block) ){
+            move_left.kill();
+            alert("COLLISION!");
+          }
+        } 
+      });
     }
     if(e.which === 37){
-      TweenLite.to( ball , 1 , {left:"-=50px"} );
-
+      var move_right = TweenLite.to( ball , 1 , { 
+        left:"-=200px",
+        onUpdate: function(){
+          if( didCollide(ball, block) ){
+            move_right.kill();
+            alert("COLLISION!");
+          }
+        }
+      });
     }
   });
 });
